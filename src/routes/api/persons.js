82288f49d4e98e35d06fbaefd10e558ca046881b
BarbaricxEric <castilloer33@yahoo.com>
2022-11-03 03:09:20 -0500

    Added Persons API Endpoint

--- src/routes/api/persons.js
+++ src/routes/api/persons.js
@@ -6,5 +6,24 @@ router.get('/' , (req,res)=>{
     res.send({msg: 'Get All Persons'})
 })
 
+router.get('/:id' , (req,res)=>{
+    res.send({msg: `Getting Person ${req.params.id} `})
+})
+
+router.post('/' , (req,res)=>{
+    res.send({msg: 'Creating a new Person'})
+})
+
+router.put('/:id' , (req,res)=>{
+    res.send({msg: `Updating Person ${req.params.id} `})
+})
+
+router.delete('/:id' , (req,res)=>{
+    res.send({msg: `Deleting Person ${req.params.id} `})
+})
+
+router.get('/:id/addresses' , (req,res)=>{
+    res.send({msg: `Getting Addresses for Person ${req.params.id} `})
+})
 
 export default router
